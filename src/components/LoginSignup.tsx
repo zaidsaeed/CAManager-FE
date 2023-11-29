d5852e6389483d31df6b38095fc12b6d57b1f48e
zaidsaeed <zsaee060@uottawa.ca>
2023-11-29 13:52:42 -0800

    changed background color of entire app

--- src/components/LoginSignup.tsx
+++ src/components/LoginSignup.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './LoginSignup.css';
 
+const user_icon = require("./Assets/person.png");
+const email_icon = require("./Assets/email.png");
+const password_icon = require("./Assets/password.png");
+
 const LoginSignUp = () => {
     return (
         <div className='container'>
@@ -11,11 +15,25 @@ const LoginSignUp = () => {
 
             <div className="inputs">
                 <div className="input">
-                    <img src="" alt="" />
+                    <img src={user_icon} alt="" />
                     <input type="text" />
                 </div>
-            </div>
 
+                <div className="input">
+                    <img src={email_icon} alt="" />
+                    <input type="email" />
+                </div>
+
+                <div className="input">
+                    <img src={password_icon} alt="" />
+                    <input type="password" />
+                </div>
+            </div>
+        <div className="forgot_password">Lost password? Click here.</div>
+        <div className="submit-container">
+            <div className="submit">Sign Up</div>
+            <div className="submit">Log in</div>
+        </div>
         </div>
       );
 }
