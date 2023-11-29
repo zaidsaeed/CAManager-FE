e5dc7546c04a7db58fb8a90f7182416523303db5
zaidsaeed <zsaee060@uottawa.ca>
2023-11-29 14:35:41 -0800

    css of the landing page looks good, need to look into js now

--- src/components/LoginSignup.tsx
+++ src/components/LoginSignup.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './LoginSignup.css';
 
 const user_icon = require("./Assets/person.png");
@@ -6,10 +6,13 @@ const email_icon = require("./Assets/email.png");
 const password_icon = require("./Assets/password.png");
 
 const LoginSignUp = () => {
+
+    const [action, setAction] = useState("Sign Up");
+
     return (
         <div className='container'>
             <div className='header'>
-                <div className='text'>Sign Up</div>
+                <div className='text'>{action}</div>
                 <div className='underline'></div>
             </div>
 
@@ -29,11 +32,11 @@ const LoginSignUp = () => {
                     <input type="password" placeholder='Password'/>
                 </div>
             </div>
-        <div className="forgot_password">Lost password? Click here.</div>
-        <div className="submit-container">
-            <div className="submit">Sign Up</div>
-            <div className="submit">Log in</div>
-        </div>
+            <div className="forgot-password">Lost password? <span>Click here.</span></div>
+            <div className="submit-container">
+                <div className={action === "Sign Up" ? "submit" : "submit gray"} onClick={() => setAction("Sign Up")}>Sign Up</div>
+                <div className={action === "Log In" ? "submit" : "submit gray"} onClick={() => setAction("Log In")}>Log in</div>
+            </div>
         </div>
       );
 }
