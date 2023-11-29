b30832e8524a632b27504c0ae5639a0fb321e643
zaidsaeed <zsaee060@uottawa.ca>
2023-11-29 13:40:21 -0800

    first commit

---
+++ src/components/LoginSignup.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './LoginSignup.css';
+
+const LoginSignUp = () => {
+    return (
+        <div className='container'>
+            <div className='header'>
+                <div className='text'>Sign Up</div>
+                <div className='underline'></div>
+            </div>
+
+            <div className="inputs">
+                <div className="input">
+                    <img src="" alt="" />
+                    <input type="text" />
+                </div>
+            </div>
+
+        </div>
+      );
+}
+ 
+export default LoginSignUp;
