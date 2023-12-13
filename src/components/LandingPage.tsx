c5c3a851247eda86392a1a0bdeb3eaae2e5a1c3e
zaidsaeed <zsaee060@uottawa.ca>
2023-12-12 23:44:11 -0800

    added a basic landing page, need to fix routing now

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,12 +1,23 @@
 import React, {useState} from 'react';
+import "./LandingPage.css";
 
 const LandingPage = () => {
 
-    return <div>
-        <h1>
-            "LandingPage"
-        </h1>
-    </div>;
+    return (
+        <React.Fragment>
+        <section>
+          <div className='wrapper'>
+            <div className="centered">
+              <h1>CAManager</h1>
+              <div className="buttons">
+                <button type="button" className="btn btn-primary btn-lg" style={{marginRight: "10px"}}>Login</button>
+                <button type="button" className="btn btn-primary btn-lg" style={{marginLeft: "15px"}}>Sign up</button>
+              </div>
+            </div>
+          </div>
+        </section>
+      </React.Fragment>
+    );
 }
  
 export default LandingPage;
