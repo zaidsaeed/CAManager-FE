16e4bdc311ce6e6ca7e549a07101964cbc770295
zaidsaeed <zsaee060@uottawa.ca>
2023-12-14 10:16:02 -0800

    completed sign up page logic, need to check why I am getting a CORS error

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -3,12 +3,10 @@ import { useNavigate } from "react-router-dom";
 import './Signup.css';
 import axios from 'axios';
 
-
-const email_icon = require("./Assets/email.png");
-const password_icon = require("./Assets/password.png");
-
 const ACCOUNT_TYPE_MANAGER = "MANAGER";
 const ACCOUNT_TYPE_ATHLETE = "ATHLETE";
+
+const API_URL = "https://camanager.onrender.com";
 
 const SignUp = () => {
     const navigate = useNavigate();
@@ -17,21 +15,48 @@ const SignUp = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [accountType, setAccountType] = useState(ACCOUNT_TYPE_MANAGER);
+    const [error, setError] = useState("");
+
+
+    const placeErrorOnInputs = (errorMessage: string): void => {
+        setError(errorMessage);
+        const inputs = document.getElementsByClassName("form-control");
+        for (var i = 0; i < inputs.length; i++) {
+            const input = inputs[i];
+            input.classList.add("is-invalid");
+        }
+    };
 
     const signUp = async (): Promise<void> => {
-        console.log(firstName);
-        console.log(lastName);
-        console.log(email);
-        console.log(password);
-        console.log(accountType);
+        const requestBody = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password
+        };
 
-        // await axios.get(`${API_URL}/api/v1/users/${email}`)
-        //     .then(res => {
-        //         console.log("res", res);
-        //         return navigate("/home");
-        //     }).catch(err => {
-        //         console.log("err", err);
-        //     });
+        let axiosConfig = {
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8',
+                "Access-Control-Allow-Origin": "*",
+            }
+        };
+
+        if (accountType === ACCOUNT_TYPE_ATHLETE) {
+            await axios.post(`${API_URL}/api/v1/athletes/`, requestBody, axiosConfig)
+                .then(res => {
+                    return navigate("/home");
+                }).catch(err => {
+                    placeErrorOnInputs(err.response.data)
+                });
+        } else {
+            await axios.post(`${API_URL}/api/v1/managers/`, requestBody, axiosConfig)
+                .then(res => {
+                    return navigate("/home");
+                }).catch(err => {
+                    placeErrorOnInputs(err.response.data)
+                });
+        }
     }
 
     return (
@@ -43,22 +68,18 @@ const SignUp = () => {
 
             <div className="su-inputs">
                 <div className="su-input">
-                    <img src={email_icon} alt="" />
                     <input className="form-control" type="text" placeholder='First Name' onChange={(e) => setFirstName(e.target.value)} value={firstName}/>
                 </div>
 
                 <div className="su-input">
-                    <img src={email_icon} alt="" />
                     <input className="form-control" type="text" placeholder='Last Name' onChange={(e) => setLastName(e.target.value)} value={lastName}/>
                 </div>
 
                 <div className="su-input">
-                    <img src={email_icon} alt="" />
                     <input className="form-control" type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)} value={email}/>
                 </div>
 
                 <div className="su-input">
-                    <img src={password_icon} alt="" />
                     <input className="form-control" type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)} value={password}/>
                 </div>
             </div>
