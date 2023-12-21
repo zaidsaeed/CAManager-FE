d27034b73a216beca415bfe69b55714afb78753e
zaidsaeed <zsaee060@uottawa.ca>
2023-12-21 15:26:25 -0800

    add bootstrap js file to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,28 @@
-import {useState} from 'react';
+import { useState } from "react";
 
 import LandingPage from "./components/LandingPage";
-import LogIn from './components/LogIn';
-import AthleteInfo from './components/AthleteInfo';
+import LogIn from "./components/LogIn";
+import AthleteInfo from "./components/AthleteInfo";
 import AppContext from "./Context";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Signup from './components/Signup';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootswatch/dist/litera/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signup from "./components/Signup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootswatch/dist/litera/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
 
 function App() {
   const [user, setUser] = useState({});
   return (
-      <AppContext.Provider value={{ user, setUser }} >
-
-        <Router>
-          <Routes>
-            <Route 
-              path="/"
-              element={<LandingPage/>}
-            />
-            <Route
-              path="/login"
-              element={<LogIn/>}
-            />
-            <Route
-              path="/signup"
-              element={<Signup/>}
-            />
-            <Route
-              path="/athleteInfo"
-              element={<AthleteInfo/>}
-            />
-          </Routes>
-        </Router>
-      </AppContext.Provider>
+    <AppContext.Provider value={{ user, setUser }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/athleteInfo" element={<AthleteInfo />} />
+        </Routes>
+      </Router>
+    </AppContext.Provider>
   );
 }
 
